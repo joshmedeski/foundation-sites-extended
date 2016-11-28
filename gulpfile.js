3891c03eac111404154cef520790668be1864278
Josh Medeski <josh@medology.com>
2016-11-28 15:38:52 -0600

    Get sass and panini working for color float and spacing

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
-const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const panini = require('panini');
 
-const paths = {
-    html: './test/visual/*.html',
-    sass: './assets/*.scss',
-    sassWatch: ['./assets/*.scss', './scss/**/*.scss'],
+const PATHS = {
+    pages: 'panini/pages/*.html',
+    panini: 'panini/**/*',
+    sass: 'panini/assets/*.scss',
+    sassWatch: ['assets/*.scss', 'scss/**/*.scss'],
     sassIncludePaths: [
-        './scss',
-        './bower_components/foundation-sites/scss'
+        'scss',
+        'bower_components/foundation-sites/scss'
     ]
 };
 
@@ -15,29 +17,40 @@ gulp.task('sass', function () {
     const sourcemaps = require('gulp-sourcemaps');
     const sass = require('gulp-sass');
 
-    return gulp.src(paths.sass)
+    return gulp.src(PATHS.sass)
         .pipe(sourcemaps.init())
         .pipe(sass({
-            includePaths: paths.sassIncludePaths
+            includePaths: PATHS.sassIncludePaths
         }).on('error', sass.logError))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./dist'))
+        .pipe(gulp.dest('./build'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('serve', ['sass'], function() {
+gulp.task('panini', function(done) {
+    gulp.src(PATHS.pages)
+        .pipe(panini({
+            root: 'panini/pages/',
+            layouts: 'panini/layouts/',
+            partials: 'panini/partials/',
+            helpers: 'panini/helpers/',
+            data: 'panini/data/'
+        }))
+        .pipe(gulp.dest('build'));
+
+    gulp.watch([PATHS.panini], [panini.refresh]);
+    done();
+});
+
+gulp.task('serve', ['sass', 'panini'], function() {
     browserSync.init({
         server: {
-            baseDir: './test/visual',
-            directory: true,
-            routes: {
-                '/dist': 'dist'
-            }
+            baseDir: 'build',
+            directory: true
         }
     });
 
-    gulp.watch(paths.sassWatch, ['sass']);
-    gulp.watch(paths.html).on('change', browserSync.reload);
+    gulp.watch(PATHS.panini, browserSync.reload);
 });
 
-gulp.task('default', ['sass', 'serve']);
+gulp.task('default', ['sass', 'panini', 'serve']);
